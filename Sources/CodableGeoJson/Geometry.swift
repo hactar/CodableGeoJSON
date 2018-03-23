a31effd7a88398516fe897cfef713870e92f4802
PW <patrick@subzero.eu>
2018-03-23 16:19:13 +0100

    - improved geo center calculation

--- Sources/CodableGeoJson/Geometry.swift
+++ Sources/CodableGeoJson/Geometry.swift
@@ -19,14 +19,33 @@ public struct Polygon: GeoJSON {
     public let coordinates: [[Position]]
     
     public func centerOfGeometry() -> Position {
-        var latitude: Float = 0.0
-        var longitude: Float = 0.0
+        var latitudeSum: Float = 0.0
+        var longitudeSum: Float = 0.0
+        var latitudeLast: Float = -1.0
+        var longitudeLast: Float = -1.0
+        var usedCoords = 0
+        var ignoredCoords = 0
         self.coordinates.first!.forEach { (position) in
-            latitude = latitude + position.latitude
-            longitude = longitude + position.longitude
+
+            
+            let distanceLat = abs(latitudeLast-position.latitude)
+            let distanceLong = abs(longitudeLast-position.longitude)
+            let roughDistance = distanceLat + distanceLong
+            //print("Rough distance is: \(roughDistance)")
+            
+            if roughDistance > 0.005 {
+                latitudeSum = latitudeSum + position.latitude
+                longitudeSum = longitudeSum + position.longitude
+                latitudeLast = position.latitude
+                longitudeLast = position.longitude
+                usedCoords = usedCoords + 1
+            } else {
+                ignoredCoords = ignoredCoords + 1
+            }
+
         }
-        let numObjects =  Float(self.coordinates.first!.count)
-        return Position(latitude: latitude / numObjects, longitude: longitude / numObjects, altitude: nil)
+        print("ignored coords: \(ignoredCoords)")
+        return Position(latitude: latitudeSum / Float(usedCoords), longitude: longitudeSum / Float(usedCoords), altitude: nil)
         
     }
     
