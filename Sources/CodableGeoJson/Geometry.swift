bac31435bf37e75f11372886f1a8b89be529d768
PW <patrick@subzero.eu>
2018-03-25 15:34:29 +0200

    - removed center point calculation as isnt wasnt good enough for irregular polys. Use polylabel instead.

--- Sources/CodableGeoJson/Geometry.swift
+++ Sources/CodableGeoJson/Geometry.swift
@@ -18,36 +18,7 @@ public struct Polygon: GeoJSON {
     /// Array of array of positions. The first subArray is the outer ring, any other arrays are cut outs.
     public let coordinates: [[Position]]
     
-    public func centerOfGeometry() -> Position {
-        var latitudeSum: Float = 0.0
-        var longitudeSum: Float = 0.0
-        var latitudeLast: Float = -1.0
-        var longitudeLast: Float = -1.0
-        var usedCoords = 0
-        var ignoredCoords = 0
-        self.coordinates.first!.forEach { (position) in
 
-            
-            let distanceLat = abs(latitudeLast-position.latitude)
-            let distanceLong = abs(longitudeLast-position.longitude)
-            let roughDistance = distanceLat + distanceLong
-            //print("Rough distance is: \(roughDistance)")
-            
-            if roughDistance > 0.005 {
-                latitudeSum = latitudeSum + position.latitude
-                longitudeSum = longitudeSum + position.longitude
-                latitudeLast = position.latitude
-                longitudeLast = position.longitude
-                usedCoords = usedCoords + 1
-            } else {
-                ignoredCoords = ignoredCoords + 1
-            }
-
-        }
-        print("ignored coords: \(ignoredCoords)")
-        return Position(latitude: latitudeSum / Float(usedCoords), longitude: longitudeSum / Float(usedCoords), altitude: nil)
-        
-    }
     
     public init(coordinates: [[Position]]) {
         self.coordinates = coordinates
